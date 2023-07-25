8d5aec4e73df8ea6d8b4552774b8c633bdddc15f
ngtuonghy <ngtuonghy@protonmail.com>
2023-07-25 09:32:02 +0700

    change

--- lua/runner-nvim/init.lua
+++ lua/runner-nvim/init.lua
@@ -3,10 +3,10 @@ Default_config = {
 	terminal = "horizontal",
 	commands = {
 		go = {
-			cmd = "go run",
+			cmd = "cd $dir && go run",
 		},
 		cpp = {
-			cmd = "g++",
+			cmd = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir $fileNameWithoutExt",
 		},
 	},
 }
@@ -19,7 +19,7 @@ end
 function M.toggle_my_feature()
 	vim.g.my_feature_enabled = not vim.g.my_feature_enabled
 	if vim.g.my_feature_enabled then
-		print("Makefile is now enabled!")
+		print("Makefile prior is now enabled!")
 	else
 		print("Makefile is now disabled!")
 	end
@@ -37,55 +37,33 @@ end
 local function get_current_filepath()
 	return vim.fn.expand("%:p:h")
 end
-
--- local function check_makefile()
--- 	local makefile_exists = vim.fn.filereadable("Makefile") == 1
--- 	if makefile_exists then
--- 		return "Thư mục hiện tại có tệp Makefile."
--- 	else
--- 		return "Thư mục hiện tại không có tệp Makefile."
--- 	end
 -- end
-
-local function cpp(config, current_file)
-	local compile_cmd = get_current_filename() .. " -o " .. get_current_filename():gsub("%.cpp", "")
-	local run_cmd = get_current_filename():gsub("%.cpp", "")
-	require("nvterm.terminal").send(
-		"cd " .. current_file .. " && " .. config.cmd .. " " .. compile_cmd .. " && " .. "./" .. run_cmd,
-		Default_config.terminal
-	)
+--	cmd = "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir $fileNameWithoutExt",
+local function changeCmd()
+	local current_filepath = get_current_filepath()
+	local current_filetype = get_filetype()
+	local current_filename = get_current_filename()
+	local current_withoutext = get_current_filename():gsub("%.cpp", "")
+	local getcmd = Default_config.commands[current_filetype].cmd
+	getcmd = string.gsub(getcmd, "$dir", current_filepath)
+	getcmd = string.gsub(getcmd, "&&", ";")
+	getcmd = string.gsub(getcmd, "$fileName", current_filename)
+	getcmd = string.gsub(getcmd, "$fileNameWithoutExt", current_withoutext)
+	return getcmd
 end
-local function rust(config, current_file)
-	local compile_cmd = get_current_filename() .. get_current_filename():gsub("%.rs", "")
-	local run_cmd = get_current_filename():gsub("%.rs", "")
-	require("nvterm.terminal").send(
-		"cd " .. current_file .. "&&" .. config.cmd .. " " .. compile_cmd .. " && " .. run_cmd,
-		Default_config.terminal
-	)
-end
-
 local function makefile(config, current_file)
 	require("nvterm.terminal").send("cd " .. current_file .. " && " .. config.Makefile, Default_config.terminal)
 end
 function M.Coderun()
 	local current_filetype = get_filetype()
-	local current_file = get_current_filepath()
-	local config = Default_config.commands[current_filetype]
-
-	if vim.g.my_feature_enabled and config and config.Makefile then
-		makefile(config, current_file)
+	local current_filepath = get_current_filepath()
+	local get_name = Default_config.commands[current_filetype]
+	if vim.g.my_feature_enabled and get_name and get_name.Makefile then
+		makefile(get_name, current_filepath)
 	else
-		if config and config.cmd then
-			if current_filetype == "cpp" then
-				cpp(config, current_file)
-			elseif current_filetype == "rust" then
-				rust(config, current_file)
-			else
-				require("nvterm.terminal").send(
-					"cd " .. current_file .. " && " .. config.cmd .. " " .. get_current_filename(),
-					Default_config.terminal
-				)
-			end
+		if get_name and get_name.cmd then
+			local get_cmd = changeCmd()
+			require("nvterm.terminal").send(get_cmd, Default_config.terminal)
 		else
 			print("No command configured for this filetype.")
 		end
